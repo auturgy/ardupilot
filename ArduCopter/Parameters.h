467eedcb3cabb8152080cc45270bd9ddfc80e4ce
auturgy <james@auturgy.com.au>
2018-03-15 15:08:21 +1100

    Revert "Copter: allow AP_Stats to be optional"
    This reverts commit ce6d51989c3590e410a12ff1092e7dfb0a6ecee8.

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -498,10 +498,8 @@ public:
     // button checking
     AP_Button button;
 
-#if STATS_ENABLED == ENABLED
     // vehicle statistics
     AP_Stats stats;
-#endif
 
 #if GRIPPER_ENABLED
     AP_Gripper gripper;
