f5d137675dddf739558d71cd7f9f368c077ab4ec
auturgy <james@auturgy.com.au>
2018-03-15 15:08:21 +1100

    Revert "AP_Param_Helper: HAL_F4Light parameters divided into common and board specific"
    This reverts commit e6016271665394c685814712798a76f234f9b83c.

--- libraries/AP_Param_Helper/AP_Param_Helper.h
+++ libraries/AP_Param_Helper/AP_Param_Helper.h
@@ -16,12 +16,7 @@ public:
 
     static const AP_Param::GroupInfo var_info[];
 
-// common HAL params
-#if defined(F4LIGHT_HAL_PARAMS)
-    F4LIGHT_HAL_PARAMS
-#endif
-    
-// per board params - only if defined
+// only if defined
 #ifdef BOARD_HAL_PARAMS
     BOARD_HAL_PARAMS
 #endif
