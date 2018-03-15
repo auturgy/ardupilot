a8819518df51df4f8bb3ea03eea853854ac166f4
auturgy <james@auturgy.com.au>
2018-03-15 15:08:21 +1100

    Revert "AP_HAL_F4Light: build changed to use bootloader also added generation of .apj images"
    This reverts commit 817de26fe08399c486e3e712095139e1579e16e9.

--- libraries/AP_HAL_F4Light/boards/f4light_Airbot/target-config.mk
+++ libraries/AP_HAL_F4Light/boards/f4light_Airbot/target-config.mk
@@ -1,14 +1,10 @@
 # Board-specific configuration values.  Flash and SRAM sizes in bytes.
 
-    MCU            := STM32F405RG
-    PRODUCT_ID     := 0003
-    DENSITY        := STM32_HIGH_DENSITY
-    FLASH_SIZE     := 1048576
-    SRAM_SIZE      := 131072
-
-    BOARD_TYPE     := 70
-    BOARD_REV      := 0
-    BOOTLOADER     := revo405_bl
+   MCU            := STM32F405RG
+   PRODUCT_ID     := 0003
+   DENSITY        := STM32_HIGH_DENSITY
+   FLASH_SIZE     := 1048576
+   SRAM_SIZE      := 131072
 
 # Memory target-specific configuration values
 
